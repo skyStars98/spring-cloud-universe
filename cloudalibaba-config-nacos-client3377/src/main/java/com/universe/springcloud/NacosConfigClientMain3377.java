1887f023a0e1f1466333bc9c6a71b6d488076c1d
daiguoqing <1998longtime>
2020-04-29 19:21:48 +0800

    加入nacos集群

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/universe/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,19 @@
+package com.universe.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: daiguoqing
+ * @Date: 2020/4/29
+ * @Time: 9:14
+ * @Version: spring-cloud-universe 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
